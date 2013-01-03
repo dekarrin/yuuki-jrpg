dceda59a72804be8e1eb139f675221f69ba50161
TF Nelson <tails.fanatic@gmail.com>
2013-01-03 13:54:10 -0600

    Added missing docs to file

--- src/yuuki/ui/MessageBoxInputListener.java
+++ src/yuuki/ui/MessageBoxInputListener.java
@@ -1,6 +1,22 @@
 package yuuki.ui;
 
+/**
+ * A listener for events fired from a MessageBox.
+ */
 public interface MessageBoxInputListener {
+	
+	/**
+	 * Fired when the user enters text into a text prompt.
+	 * 
+	 * @param input The text that the user entered.
+	 */
 	public void enterClicked(String input);
+	
+	/**
+	 * Fired when the user selects a choice from a choice prompt.
+	 * 
+	 * @param option The option that the user selected.
+	 */
 	public void optionClicked(Object option);
+	
 }
