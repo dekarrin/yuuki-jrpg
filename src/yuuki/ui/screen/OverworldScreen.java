9a6d24a9e2f6dc6604c034ddbd963b2d1dab20b9
TF Nelson <tails.fanatic@gmail.com>
2013-01-27 19:07:37 -0600

    Added movement buttons; no functionality yet

--- src/yuuki/ui/screen/OverworldScreen.java
+++ src/yuuki/ui/screen/OverworldScreen.java
@@ -1,6 +1,8 @@
 package yuuki.ui.screen;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.event.KeyAdapter;
@@ -39,6 +41,51 @@ MouseListener {
 	private JButton startButton;
 	
 	/**
+	 * The button that moves the character north.
+	 */
+	private JButton moveNorthButton;
+	
+	/**
+	 * The button that moves the character west.
+	 */
+	private JButton moveWestButton;
+	
+	/**
+	 * The button that moves the character east.
+	 */
+	private JButton moveEastButton;
+	
+	/**
+	 * The button that moves the character south.
+	 */
+	private JButton moveSouthButton;
+	
+	/**
+	 * The button that moves the character north-east.
+	 */
+	private JButton moveNorthEastButton;
+	
+	/**
+	 * The button that moves the character north-west.
+	 */
+	private JButton moveNorthWestButton;
+	
+	/**
+	 * The button that moves the character south-east.
+	 */
+	private JButton moveSouthEastButton;
+	
+	/**
+	 * The button that moves the character south-west.
+	 */
+	private JButton moveSouthWestButton;
+	
+	/**
+	 * The button that makes the character pass one turn.
+	 */
+	private JButton moveNullButton;
+	
+	/**
 	 * Displays the world.
 	 */
 	private WorldViewer worldViewer;
@@ -61,12 +108,72 @@ MouseListener {
 			}
 		};
 		setLayout(new FlowLayout());
-		Box vertBox = Box.createVerticalBox();
-		Box fieldBox = Box.createHorizontalBox();
 		worldViewer = new WorldViewer(VIEWER_WIDTH, VIEWER_HEIGHT);
 		startButton = new JButton("Start");
 		startButton.addMouseListener(this);
 		startButton.addKeyListener(enterListener);
+		createMovementButtons();
+		addElements();
+	}
+	
+	/**
+	 * Constructs the movement buttons.
+	 */
+	private void createMovementButtons() {
+		moveNorthButton = new JButton("\u2191"); // arrow char
+		moveWestButton = new JButton("\u2190"); // arrow char
+		moveEastButton = new JButton("\u2192"); // arrow char
+		moveSouthButton = new JButton("\u2193"); // arrow char
+		moveNorthEastButton = new JButton("\u2197"); // arrow char
+		moveNorthWestButton = new JButton("\u2196"); // arrow char
+		moveSouthEastButton = new JButton("\u2198"); // arrow char
+		moveSouthWestButton = new JButton("\u2199"); // arrow char
+		moveNullButton = new JButton("\u25CF"); // dot char
+	}
+	
+	/**
+	 * Creates the box containing the movement buttons.
+	 * 
+	 * @return The box containing the movement buttons.
+	 */
+	private Box createMovementBox() {
+		Dimension d = new Dimension(170, 100);
+		Box moveBox = Box.createVerticalBox();
+		moveBox.setPreferredSize(d);
+		moveBox.setMaximumSize(d);
+		Box row1 = Box.createHorizontalBox();
+		Box row2 = Box.createHorizontalBox();
+		Box row3 = Box.createHorizontalBox();
+		row1.add(moveNorthWestButton);
+		row1.add(Box.createHorizontalGlue());
+		row1.add(moveNorthButton);
+		row1.add(Box.createHorizontalGlue());
+		row1.add(moveNorthEastButton);
+		row2.add(moveWestButton);
+		row2.add(Box.createHorizontalGlue());
+		row2.add(moveNullButton);
+		row2.add(Box.createHorizontalGlue());
+		row2.add(moveEastButton);
+		row3.add(moveSouthWestButton);
+		row3.add(Box.createHorizontalGlue());
+		row3.add(moveSouthButton);
+		row3.add(Box.createHorizontalGlue());
+		row3.add(moveSouthEastButton);
+		moveBox.add(row1);
+		moveBox.add(Box.createVerticalGlue());
+		moveBox.add(row2);
+		moveBox.add(Box.createVerticalGlue());
+		moveBox.add(row3);
+		return moveBox;
+	}
+	
+	/**
+	 * Creates element containers and adds them to this Screen.
+	 */
+	private void addElements() {
+		Box moveBox = createMovementBox();
+		Box vertBox = Box.createVerticalBox();
+		Box fieldBox = Box.createHorizontalBox();
 		fieldBox.add(new JLabel("The overworld!"));
 		fieldBox.add(Box.createHorizontalStrut(2));
 		fieldBox.add(new JLabel("Hit the button to start a battle ==>"));
@@ -74,6 +181,8 @@ MouseListener {
 		fieldBox.add(startButton);
 		vertBox.add(worldViewer);
 		vertBox.add(fieldBox);
+		vertBox.add(Box.createVerticalStrut(10));
+		vertBox.add(moveBox);
 		add(vertBox);
 	}
 	
