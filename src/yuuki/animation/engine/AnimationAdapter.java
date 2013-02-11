986f19e09890af06a2e06743a53fb802eb360766
TF Nelson <tails.fanatic@gmail.com>
2013-02-11 17:07:49 -0600

    Cleaned source and fixed #48

--- src/yuuki/animation/engine/AnimationAdapter.java
+++ src/yuuki/animation/engine/AnimationAdapter.java
@@ -4,10 +4,10 @@ package yuuki.animation.engine;
  * Provides empty implementations of AnimationListener.
  */
 public class AnimationAdapter implements AnimationListener {
-
+	
 	@Override
 	public void animationComplete(AnimationEvent e) {}
-
+	
 	@Override
 	public void animationStopped(AnimationEvent e) {}
 	
