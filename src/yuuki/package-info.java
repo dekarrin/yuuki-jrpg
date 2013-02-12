ea8b1dc03fbe6991d316e5167247ff710157d48b
TF Nelson <tails.fanatic@gmail.com>
2013-02-12 05:51:02 -0600

    Added descriptive documentation to package yuuki.

--- src/yuuki/package-info.java
+++ src/yuuki/package-info.java
@@ -1,4 +1,6 @@
 /**
- * Contains the executable Engine class and other supporting classes.
+ * Contains the main game classes. The yuuki package includes the executable
+ * {@link Engine} class, which drives all other game components, and the
+ * {@link Options} class, which contains all the options for the game.
  */
 package yuuki;
