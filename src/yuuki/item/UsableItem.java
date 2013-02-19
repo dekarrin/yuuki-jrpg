1c50f714e6080b6e6a17eecae57f319e0596b377
TF Nelson <tails.fanatic@gmail.com>
2013-02-19 10:57:45 -0600

    Added item classes

---
+++ src/yuuki/item/UsableItem.java
@@ -0,0 +1,84 @@
+package yuuki.item;
+
+import yuuki.action.Action;
+
+/**
+ * An item that executes some action at no cost with a 100% success rate.
+ */
+public class UsableItem extends Item {
+	
+	/**
+	 * The action that this Item executes.
+	 */
+	private final Action action;
+	
+	/**
+	 * The number of times that this Item may be used.
+	 */
+	private final int maxUses;
+	
+	/**
+	 * The number of times that this Item has been used.
+	 */
+	private int uses;
+	
+	/**
+	 * Creates a new UsableItem.
+	 * 
+	 * @param name The name of the item.
+	 * @param value The value of the item.
+	 * @param action The action that using this item performs.
+	 * @param uses The number of times that this item may be used.
+	 */
+	public UsableItem(String name, int value, Action action, int uses) {
+		super(name, value);
+		this.action = action;
+		this.maxUses = uses;
+		this.uses = 0;
+	}
+	
+	/**
+	 * Gets the action that this item performs, which counts as a use of this
+	 * Item.
+	 * 
+	 * @return The Action.
+	 */
+	public Action getActionForUse() {
+		uses++;
+		if (uses > maxUses) {
+			throw new IllegalStateException("Item used too many times");
+		}
+		return action;
+	}
+	
+	/**
+	 * Gets the maximum number of uses for this item.
+	 * 
+	 * @return The maximum number of uses.
+	 */
+	public int getMaxUses() {
+		return maxUses;
+	}
+	
+	/**
+	 * Gets the use count for this item.
+	 * 
+	 * @return The current number of uses.
+	 */
+	public int getUses() {
+		return uses;
+	}
+	
+	/**
+	 * Sets the use count for this item. This is bounded by the maxUses.
+	 * 
+	 * @param uses What to set the use count to.
+	 */
+	public void setUses(int uses) {
+		if (uses < 0 || uses > maxUses) {
+			throw new IllegalArgumentException(uses + " is out of range");
+		}
+		this.uses = uses;
+	}
+	
+}
