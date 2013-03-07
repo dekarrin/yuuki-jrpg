a2316fefb150a856581dfc6658ed8cc4e06244f8
TF Nelson <tails.fanatic@gmail.com>
2013-03-07 07:21:46 -0600

    Added panels to the mod-loading dialog in options

--- src/yuuki/ui/ModPanel.java
+++ src/yuuki/ui/ModPanel.java
@@ -1,21 +1,34 @@
 package yuuki.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  * Shows mods that have been enabled, and allows the user to enable/disable
  * them.
  */
+@SuppressWarnings("serial")
 public class ModPanel extends JPanel {
 	
-	private static final long serialVersionUID = 6077820699428016095L;
-
+	/**
+	 * The minimum height of a mod sub-panel.
+	 */
+	private static final int MIN_MOD_HEIGHT = 20;
+	
+	/**
+	 * The list that contains all individual mods.
+	 */
+	private JPanel modList;
+	
 	/**
 	 * Creates a new ModPanel.
 	 * 
@@ -28,23 +41,123 @@ public class ModPanel extends JPanel {
 		setMinimumSize(size);
 		setMaximumSize(size);
 		setSize(size);
-		final int SIZE = 11;
-		Integer[] nums = new Integer[SIZE];
-		for (int i = 0; i < SIZE; i++) {
-			nums[i] = i + 1;
-		}
-		@SuppressWarnings({"rawtypes", "unchecked"})
-		JList list = new JList(nums);
-		list.setPreferredSize(size);
-		list.setMinimumSize(new Dimension(width, 1));
-		Box layout = Box.createVerticalBox();
-		layout.setPreferredSize(size);
-		layout.setMinimumSize(size);
-		layout.setMaximumSize(size);
-		layout.setSize(size);
-	//	layout.add(new JLabel("Mods:"));
-		add(layout);
-		layout.add(list);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		createComponents();
+		addComponents(width, height);
+		addMod("Super cool awesome funtime");
+	}
+	
+	/**
+	 * Adds a mod to the displayed list.
+	 * 
+	 * @param name The name of the mod.
+	 */
+	public void addMod(String name) {
+		JPanel modPanel = createModPanel(name);
+		modList.add(modPanel);
+	}
+	
+	/**
+	 * Adds all child components to this ModPanel.
+	 * 
+	 * @param width The width of this ModPanel.
+	 * @param height The height of this ModPanel.
+	 */
+	private void addComponents(int width, int height) {
+		JPanel labelPanel = createLabelPanel(width, "Mods:");
+		JScrollPane modListView = createViewPort(width, height);
+		add(labelPanel);
+		add(Box.createVerticalStrut(2));
+		add(modListView);
+	}
+	
+	/**
+	 * Creates the child components of this ModPanel that are properties of it.
+	 */
+	private void createComponents() {
+		modList = new JPanel();
+		modList.setBackground(Color.LIGHT_GRAY);
+		modList.setLayout(new BoxLayout(modList, BoxLayout.Y_AXIS));
+	}
+	
+	/**
+	 * Creates a panel with center alignment containing a label with left
+	 * alignment.
+	 * 
+	 * @param width The width of the panel.
+	 * @param text The text of the label.
+	 * @return The created panel.
+	 */
+	private JPanel createLabelPanel(int width, String text) {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(text);
+		label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+		panel.setMinimumSize(new Dimension(width, 1));
+		panel.setLayout(new BorderLayout());
+		panel.add(label, BorderLayout.WEST);
+		return panel;
+	}
+	
+	/**
+	 * Creates a panel with an enabler check box for a mod.
+	 * 
+	 * @return The created enabler panel.
+	 */
+	private JPanel createModEnablerPanel() {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel("Enable:");
+		JCheckBox box = new JCheckBox();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		panel.add(label);
+		panel.add(box);
+		return panel;
+	}
+	
+	/**
+	 * Creates a panel for a mod.
+	 * 
+	 * @param name The name of the mod.
+	 * @return The created mod panel.
+	 */
+	private JPanel createModPanel(String name) {
+		JPanel panel = new JPanel();
+		JPanel title = createModTitlePanel(name);
+		JPanel enabler = createModEnablerPanel();
+		Dimension s = getPreferredSize();
+		Dimension pSize = new Dimension(s.width, MIN_MOD_HEIGHT);
+		panel.setPreferredSize(pSize);
+		panel.setMinimumSize(pSize);
+		panel.setLayout(new BorderLayout());
+		panel.add(title, BorderLayout.WEST);
+		panel.add(enabler, BorderLayout.EAST);
+		return panel;
+	}
+	
+	/**
+	 * Creates a title panel for a mod.
+	 * 
+	 * @param name The name of the mod.
+	 * @return The created title panel.
+	 */
+	private JPanel createModTitlePanel(String name) {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(name);
+		panel.add(label);
+		return panel;
+	}
+	
+	/**
+	 * Creates the view port for the mod list.
+	 * 
+	 * @param width The width of the view port.
+	 * @param height The height of the view port.
+	 * @return The created view port.
+	 */
+	private JScrollPane createViewPort(int width, int height) {
+		JScrollPane view = new JScrollPane(modList);
+		view.setPreferredSize(new Dimension(width, height));
+		view.setMinimumSize(new Dimension(width, 1));
+		return view;
 	}
 	
 }
