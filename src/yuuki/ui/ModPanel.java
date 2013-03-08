81e0068a011d98ed8d3ea7c7a6f1e107f29b1979
TF Nelson <tails.fanatic@gmail.com>
2013-03-08 15:47:35 -0600

    Added events to ModPanel

--- src/yuuki/ui/ModPanel.java
+++ src/yuuki/ui/ModPanel.java
@@ -5,6 +5,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Rectangle;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
@@ -142,9 +146,19 @@ public class ModPanel extends JPanel {
 	private static final Color MOD_COLOR_MAIN = new Color(0xc9c9c9);
 	
 	/**
+	 * Calls methods on this object when a mod is enabled/disabled.
+	 */
+	private ModControlListener listener = null;
+	
+	/**
 	 * Whether the next mod to be added should have alt colors.
 	 */
 	private boolean modAltScheme;
+	
+	/**
+	 * Maps the enablers to mod identifiers.
+	 */
+	private Map<Object, String> modIndexes = new HashMap<Object, String>();
 	
 	/**
 	 * The list that contains all individual mods.
@@ -174,13 +188,14 @@ public class ModPanel extends JPanel {
 	 * Adds a mod to the displayed list.
 	 * 
 	 * @param name The name of the mod.
+	 * @param id A string that uniquely identifies the mod.
 	 */
-	public void addMod(String name) {
+	public void addMod(String name, String id) {
 		if (!modsAdded) {
 			modList.removeAll();
 			modsAdded = true;
 		}
-		JPanel modPanel = createModPanel(name);
+		JPanel modPanel = createModPanel(name, id);
 		modList.add(modPanel);
 	}
 	
@@ -188,10 +203,20 @@ public class ModPanel extends JPanel {
 	 * Clears all mods from the list.
 	 */
 	public void clearMods() {
+		modIndexes.clear();
 		modList.removeAll();
 		modList.add(createNoModsPanel());
 		modsAdded = false;
 		modAltScheme = false;
+	}
+	
+	/**
+	 * Sets the listener for mod control events.
+	 * 
+	 * @param listener The listener.
+	 */
+	public void setModListener(ModControlListener listener) {
+		this.listener = listener;
 	}
 	
 	/**
@@ -219,6 +244,27 @@ public class ModPanel extends JPanel {
 	}
 	
 	/**
+	 * Creates a listener for a mod enabler check box.
+	 * 
+	 * @return The created listener.
+	 */
+	private ItemListener createEnablerListener() {
+		return new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				String id = modIndexes.get(e.getSource());
+				if (id != null) {
+					if (e.getStateChange() == ItemEvent.SELECTED) {
+						fireModEnabled(id);
+					} else {
+						fireModDisabled(id);
+					}
+				}
+			}
+		};
+	}
+	
+	/**
 	 * Creates a panel with center alignment containing a label with left
 	 * alignment.
 	 * 
@@ -237,12 +283,15 @@ public class ModPanel extends JPanel {
 	/**
 	 * Creates a panel with an enabler check box for a mod.
 	 * 
+	 * @param id A unique identifier for the mod.
 	 * @return The created enabler panel.
 	 */
-	private JPanel createModEnablerPanel() {
+	private JPanel createModEnablerPanel(String id) {
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Enable:");
 		JCheckBox box = new JCheckBox();
+		modIndexes.put(box, id);
+		box.addItemListener(createEnablerListener());
 		box.setOpaque(false);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 		panel.setOpaque(false);
@@ -257,12 +306,13 @@ public class ModPanel extends JPanel {
 	 * Creates a panel for a mod.
 	 * 
 	 * @param name The name of the mod.
+	 * @param id A unique identifier for the mod.
 	 * @return The created mod panel.
 	 */
-	private JPanel createModPanel(String name) {
+	private JPanel createModPanel(String name, String id) {
 		JPanel panel = new JPanel();
 		JPanel title = createModTitlePanel(name);
-		JPanel enabler = createModEnablerPanel();
+		JPanel enabler = createModEnablerPanel(id);
 		Dimension s = modList.getPreferredScrollableViewportSize();
 		panel.setPreferredSize(new Dimension(s.width, MOD_HEIGHT));
 		panel.setLayout(new BorderLayout());
@@ -316,6 +366,28 @@ public class ModPanel extends JPanel {
 		JScrollPane view = new JScrollPane(modList);
 		view.getViewport().setBackground(new Color(0xb8b8b8));
 		return view;
+	}
+	
+	/**
+	 * Calls modDisabled() on the listener.
+	 * 
+	 * @param id The ID of the disabled mod.
+	 */
+	private void fireModDisabled(String id) {
+		if (listener != null) {
+			listener.modDisabled(id);
+		}
+	}
+	
+	/**
+	 * Calls modEnabled() on the listener.
+	 * 
+	 * @param id The ID of the enabled mod.
+	 */
+	private void fireModEnabled(String id) {
+		if (listener != null) {
+			listener.modEnabled(id);
+		}
 	}
 	
 }
