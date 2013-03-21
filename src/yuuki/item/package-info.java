433f22d6f0790e4cfdfe63199378920b14be1620
TF Nelson <tails.fanatic@gmail.com>
2013-03-21 11:59:00 -0500

    Continued with item documentation

--- src/yuuki/item/package-info.java
+++ src/yuuki/item/package-info.java
@@ -2,5 +2,7 @@
  * Contains classes relating to the Item system.
  * 
  * {@link yuuki.item.Item} is the base class for all items.
+ * {@link yuuki.item.ExternallyUsableItem} is an item that may be used outside
+ * of battle.
  */
 package yuuki.item;
